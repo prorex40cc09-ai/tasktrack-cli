041948814eaf8cf76d26beea4a2ce39905c82a43
prorex40cc09-ai <prorex40cc.09@gmail.com>
2026-03-09 10:41:34 +0200

    Update main.java

--- src/main/java/com/main.java
+++ src/main/java/com/main.java
@@ -1,1 +1,14 @@
+package com.tasktrack;
 
+public class Main {
+
+    public static void main(String[] args) {
+
+        TaskManager manager = new TaskManager();
+
+        manager.addTask("Learn GitHub");
+        manager.addTask("Finish Java project");
+
+        manager.listTasks();
+    }
+}
