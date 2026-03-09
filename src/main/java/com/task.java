a0294981ab39bb5cb1fd396e0c7b75e5682c98b2
prorex40cc09-ai <prorex40cc.09@gmail.com>
2026-03-09 10:42:00 +0200

    Update task.java

--- src/main/java/com/task.java
+++ src/main/java/com/task.java
@@ -1,1 +1,24 @@
+package com.tasktrack;
 
+public class Task {
+
+    private String description;
+    private boolean completed;
+
+    public Task(String description) {
+        this.description = description;
+        this.completed = false;
+    }
+
+    public void complete() {
+        this.completed = true;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+}
